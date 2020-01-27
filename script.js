223b5c3ce6ac152fb2f837f276b7110acce4451a
mateuszklos37 <53402777+mateuszklos37@users.noreply.github.com>
2020-01-27 20:35:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://sleepy-shore-82591.herokuapp.com/v1/task';
+  var apiRoot = 'https://sleepy-shore-82591.herokuapp.com/v1/task';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
