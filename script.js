5e1b0bcc8496ec5d3987cbb60ccf8b6714d01f68
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2020-02-09 08:46:58 +0100

    Trelloroot changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://sleepy-shore-82591.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://api.trello.com/1/v1/trello/';
+  const trelloApiRoot = 'https://sleepy-shore-82591.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
