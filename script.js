e08028b05fd1c157e6fc215602ffb08f951c2a46
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2020-03-09 20:30:47 +0100

    Deploy

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://sleepy-shore-82591/v1/task/';
+  const trelloApiRoot = 'https://sleepy-shore-82591/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
